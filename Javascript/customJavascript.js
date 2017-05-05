8ea8425583b60d24511e7ea800ad2eeb6ae665f7
Abel2Code <asalinaswc1@aol.com>
2017-05-05 16:29:19 -0700

    Changed javascript navbar logic

--- Javascript/customJavascript.js
+++ Javascript/customJavascript.js
@@ -1,19 +1,5 @@
 function writeNavbar(activePage){
-  var navbar = "<div class=\"navbar-wrapper\"\> " +
-        "<div class=\"container\"\> " +
-          "<nav class=\"navbar navbar-inverse navbar-static-top\"\> " +
-            "<div class=\"container\"\> " +
-              "<div  class=\"navbar-header\"\> " +
-                "<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\"\>  " +
-                  "<span class=\"sr-only\"\>Toggle navigation</span\> " +
-                  "<span class=\"icon-bar\"\></span\> " +
-                  "<span class=\"icon-bar\"\></span\> " +
-                  "<span class=\"icon-bar\"\></span\> " +
-                "</button\> " +
-                "<a class=\"navbar-brand\" href=\"#\"\>Real Fake Doors</a\> " +
-              "</div\> " +
-              "<div id=\"navbar\" class=\"navbar-collapse collapse\"\> " +
-                "<ul class=\"nav navbar-nav\"\>" +
+  var navbar =   "<ul class=\"nav navbar-nav\"\>" +
                   "<li id=\"indexNavbar\"\><a href=\"index.html\"\>Home</a\></li\> " +
                   "<li id=\"doorPricesNavbar\"\><a href=\"doorPrices.html\"\>Door Prices</a\></li\> " +
                   "<li id=\"purchaseNavbar\"\><a href=\"purchase.html\"\>Purchase Here</a\></li\> " +
@@ -24,13 +10,8 @@ function writeNavbar(activePage){
                       "<li\><a href=\"creditCardInfo.html\"\>Sponsor2</a\></li\>" +
                     "</ul> " +
                 "</li> " +
-              "</ul> " +
-            "</div> " +
-          "</div> " +
-        "</nav> " +
-      "</div> " +
-    "</div>";
-  document.getElementById('navbarDiv').innerHTML = navbar;
+              "</ul> ";
+  document.getElementById('navbar').innerHTML = navbar;
   if(activePage == 'index'){
     document.getElementById('indexNavbar').classList.add("active");
   } else if(activePage == 'doorPrices'){
